dcea0bfd9005d4d6f038e0a8538aec65580b451f
Grey Ballard <ballard@wfu.edu>
2019-10-03 00:19:42 -0400

    added starter sequential code

---
+++ mergesort.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <ctime>
+#include <omp.h>
+using namespace std;
+
+void mergesort(int* a, int* tmp, int n);
+int medianofunion(int* a, int n, int* b, int m);
+void recmerge(int* a, int n, int* b, int m, int* tmp);
+
+int main(int argc, char** argv) {
+
+    // read inputs
+    if(argc != 4) {
+        cout << "Usage: ./a.out seed length nthreads" << endl;
+        return 1;
+    }
+    srand(atoi(argv[1]));
+    int length = atoi(argv[2]);
+    omp_set_num_threads(atoi(argv[3]));
+
+    // allocate memory
+    int* v = new int[length]; // array to be sorted
+    int* t = new int[length]; // temporary workspace
+    int* v2 = new int[length]; // copy for checking
+
+    // initialize input randomly
+    for (int i = 0; i < length; i++) {
+        v[i] = v2[i] = rand() % length;
+    }
+
+    // sort array using mergesort (and time it)
+    double start_mergeSort = omp_get_wtime();
+    mergesort(v, t, length);
+    double elapsed_mergeSort = omp_get_wtime() - start_mergeSort;
+
+    // sort array using STL (and time it)
+    double start_sort = omp_get_wtime();
+    sort(v2,v2+length);
+    double elapsed_sort = omp_get_wtime() - start_sort;
+
+    // check outputs, make sure they match
+    for(int i = 0; i < length; i++) {
+        assert(v[i] == v2[i]);
+    }
+
+    cout << "time: " << elapsed_mergeSort << '\t' << elapsed_sort << endl;
+
+    delete [] v2;
+    delete [] t;
+    delete [] v;
+
+}
+
+// sorts array a of length n, tmp is workspace of length n
+void mergesort(int* a, int* tmp, int n)
+{
+    if( n > 1 ) {
+        // sort left and right recursively
+        int mid = n / 2;
+        mergesort(a, tmp, mid);
+        mergesort(a + mid, tmp + mid, n - mid);
+
+        // merge left and right into tmp and copy back into a (using STL)
+        merge(a, a+mid, a+mid, a+n, tmp);
+        copy(tmp,tmp+n,a);
+    }
+}
+    
+// computes median of union of array a of length n and array b of length m
+// assuming elements of a and b are already internally sorted
+//   if m+n is even it returns one of the two middle values
+int medianofunion(int *a, int n, int *b, int m) {
+    int min_index = 0, max_index = n, i, j;
+    while (min_index <= max_index) {
+        i = (min_index + max_index) / 2;
+        j = ((n + m + 1) / 2) - i;
+        // Searching on right
+        if (i < n && j > 0 && b[j - 1] > a[i])
+            min_index = i + 1;
+        // Searching on left
+        else if (i > 0 && j < m && b[j] < a[i - 1])
+            max_index = i - 1;
+        // Found desired halves
+        else {
+            // no elements in 1st half of a
+            if (i == 0) return b[j - 1];
+            // no elements in 1st half of b
+            else if (j == 0) return a[i - 1];
+            else return max(a[i - 1], b[j - 1]);
+        }
+    }
+}
